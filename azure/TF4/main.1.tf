ae44b7c0f21c439e4c71b37aba8e97a5921f9e0d
Arie Heinrich <arie.heinrich@outlook.com>
2024-02-03 22:50:43 +0100

    change variable names to match component name

--- azure/TF4/main.1.tf
+++ azure/TF4/main.1.tf
@@ -36,30 +36,30 @@ module "key_vault" {
 # App Module
 module "app" {
   source                       = "../components/app"
-  component_project_prefix     = var.project_prefix
-  component_project_location   = var.project_location_prefix
-  component_environment_prefix = var.environment_prefix
-  component_location           = var.project_location
-  component_resource_group     = azurerm_resource_group.rg.name
-  component_sql_server_name    = module.db.server_name
-  component_sql_database_name  = module.db.sql_database_name
-  component_kv_id              = module.key_vault.kv_id
-  component_os_name            = "windows"
-  component_sku_name           = var.plan_sku_name
-  component_worker_count       = var.plan_worker_count
-  component_tags               = local.resource_tags
+  app_project_prefix     = var.project_prefix
+  app_project_location   = var.project_location_prefix
+  app_environment_prefix = var.environment_prefix
+  app_location           = var.project_location
+  app_resource_group     = azurerm_resource_group.rg.name
+  app_sql_server_name    = module.db.server_name
+  app_sql_database_name  = module.db.sql_database_name
+  app_kv_id              = module.key_vault.kv_id
+  app_os_name            = "windows"
+  app_sku_name           = var.plan_sku_name
+  app_worker_count       = var.plan_worker_count
+  app_tags               = local.resource_tags
 }
 
 # DB Module
 module "db" {
   source                       = "../components/db"
-  component_project_prefix     = var.project_prefix
-  component_project_location   = var.project_location_prefix
-  component_environment_prefix = var.environment_name
-  component_location           = var.project_location
-  component_resource_group     = azurerm_resource_group.rg.name
-  component_kv_id              = module.key_vault.kv_id
-  component_sql_sku            = var.db_sql_sku
-  component_sql_max_size       = var.db_sql_max_size
-  component_tags               = local.resource_tags
+  db_project_prefix     = var.project_prefix
+  db_project_location   = var.project_location_prefix
+  db_environment_prefix = var.environment_name
+  db_location           = var.project_location
+  db_resource_group     = azurerm_resource_group.rg.name
+  db_kv_id              = module.key_vault.kv_id
+  db_sql_sku            = var.db_sql_sku
+  db_sql_max_size       = var.db_sql_max_size
+  db_tags               = local.resource_tags
 }
