5709c556791b2bbe5bef3e851ff01d8f53bbb338
Arie Heinrich <arie.heinrich@outlook.com>
2024-10-09 00:02:10 +0200

    add foreach example

---
+++ azure/TF2/main.2.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "1.9.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  for_each = toset(var.resource_group_names)
+  name     = each.value
+  location = var.resource_group_location
+}
+
+variable "resource_group_location" {
+  description = "The Resource Group Location"
+  type        = string
+  default     = "North Europe"
+}
+
+variable "resource_group_names" {
+  description = "The Names of all Resource Groups"
+  type        = list(string)
+  default     = ["RG1", "RG2", "RG3"]
+}
