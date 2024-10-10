deb9ea3d98724aa302f1c8c2d69d3c1790d8b3b0
Arie Heinrich <arie.heinrich@outlook.com>
2024-10-10 12:07:36 +0200

    update the properties due to resource changes

--- azure/TF5/enterprise/modules/service_plan/main.tf
+++ azure/TF5/enterprise/modules/service_plan/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "plan" {
   name                = "${var.plan_project_prefix}-${var.plan_project_location_prefix}-${var.plan_environment_prefix}-plan"
   location            = var.plan_location
   resource_group_name = var.plan_resource_group
-  os_name             = var.plan_os_name
+  os_type             = var.plan_os_type
   sku_name            = var.plan_sku_name
   worker_count        = var.plan_worker_count
   tags                = var.plan_tags
