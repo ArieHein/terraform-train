5c7c6f95e47af8eb446974da63dc24c132f004b7
Arie Heinrich <arie.heinrich@outlook.com>
2024-10-15 23:16:16 +0200

    add windows function app to its own folder

---
+++ azure/TF4/modules/windows_function_app/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "=1.9.7"
+}
+
+resource "azurerm_function_app" "func" {
+  name                       = "${var.func_name}-plan"
+  resource_group_name        = var.func_resource_group
+  location                   = var.func_location
+  app_service_plan_id        = var.func_plan_id
+  storage_account_name       = var.func_sta_name
+  storage_account_access_key = var.func_sta_access_key
+  os_type                    = var.func_os_type
+  tags                       = var.func_tags
+}
